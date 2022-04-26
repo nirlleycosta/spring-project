fc792c1ae66a5c3339047a5758032a0c5508e9f6
Nirlley Costa <nirlleycosta@github.com>
2022-04-26 03:18:37 -0300

    Payment one-to-one association

--- src/main/java/com/nirlleycosta/course/config/TestConfig.java
+++ src/main/java/com/nirlleycosta/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.nirlleycosta.course.entities.Category;
 import com.nirlleycosta.course.entities.Order;
 import com.nirlleycosta.course.entities.OrderItem;
+import com.nirlleycosta.course.entities.Payment;
 import com.nirlleycosta.course.entities.Product;
 import com.nirlleycosta.course.entities.User;
 import com.nirlleycosta.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
