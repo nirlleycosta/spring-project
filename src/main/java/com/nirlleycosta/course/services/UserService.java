2a3c5baa825216dacc20f1acad82321e48e84d3c
Nirlley Costa <nirlleycosta@github.com>
2022-04-29 01:15:04 -0300

    Exception handling - update

--- src/main/java/com/nirlleycosta/course/services/UserService.java
+++ src/main/java/com/nirlleycosta/course/services/UserService.java
@@ -3,7 +3,10 @@ package com.nirlleycosta.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.nirlleycosta.course.entities.User;
@@ -30,13 +33,21 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
